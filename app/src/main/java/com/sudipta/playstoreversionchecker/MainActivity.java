73ea3f9d5b1dc2a25ccec6bff87514009ea1fee8
Sudipta <sudiptauem93@gmail.com>
2020-03-25 15:25:50 +0530

    update code

--- app/src/main/java/com/sudipta/playstoreversionchecker/MainActivity.java
+++ app/src/main/java/com/sudipta/playstoreversionchecker/MainActivity.java
@@ -1,20 +1,48 @@
 package com.sudipta.playstoreversionchecker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sudipta.versionchecker.PlayStoreVersionChecker;
 
 
 public class MainActivity extends AppCompatActivity {
-    PlayStoreVersionChecker playStoreVersionChecker;
+   PlayStoreVersionChecker playStoreVersionChecker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playStoreVersionChecker=new PlayStoreVersionChecker(MainActivity.this);
-        playStoreVersionChecker.VersionChecker("SUDIPTA");
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+       playStoreVersionChecker=new PlayStoreVersionChecker(MainActivity.this,getApplicationContext().getPackageName());
+       playStoreVersionChecker.VersionChecker();
+
+       getVersionFromPlayStore();
 
     }
+
+    private void getVersionFromPlayStore() {
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String verSionCode= playStoreVersionChecker.getPlaystoreVersion();
+                if (verSionCode.equals("")){
+                    System.out.println("ssssssssss"+verSionCode);
+                    getVersionFromPlayStore();
+                }
+                else {
+                    System.out.println("ssssssssss"+verSionCode);
+                }
+              //  System.out.println("ssssssssss"+verSionCode);
+            }
+        },2000);
+    }
+
 }
--- versionchecker/src/main/java/com/sudipta/versionchecker/PlayStoreVersionChecker.java
+++ versionchecker/src/main/java/com/sudipta/versionchecker/PlayStoreVersionChecker.java
@@ -1,19 +1,73 @@
 package com.sudipta.versionchecker;
 
+import android.app.Activity;
 import android.content.Context;
-import android.widget.Toast;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class PlayStoreVersionChecker {
 
     Context c;
+    String packageName;
+    WebView webView;
+    String versionCode="";
 
-     public PlayStoreVersionChecker(Context c){
-        this.c=c;
+    public PlayStoreVersionChecker(Activity c,String packageName) {
+        this.c = c;
+        this.packageName = packageName;
+        webView = new WebView(c);
     }
 
-    public void VersionChecker(String message){
+    public void VersionChecker() {
+        Log.d("ssss",packageName);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        //webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        webView.addJavascriptInterface(new MyJavaScriptInterface(), "HtmlViewer");
+        webView.loadUrl("https://play.google.com/store/apps/details?id=com.doinandin&hl=en");
+        webView.setWebViewClient(new MyBrowser());
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+    }
+
+    public String getPlaystoreVersion(){
+
+        Log.d("sssss",versionCode);
+        return versionCode;
+    }
+
+   public class MyJavaScriptInterface {
+
+        @JavascriptInterface
+        public void showHTML(String html) {
+            Log.v("showHTML", "showHTML: " + html);
+            versionCode=html;
+
+        }
 
     }
+
+   public class MyBrowser extends WebViewClient {
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            webView.loadUrl("javascript:window.HtmlViewer.showHTML(document.querySelector('div.hAyfc:nth-child(4)>span.htlgb>.IQ1z0d>.htlgb').innerHTML);");
+        }
+    }
+
 }
